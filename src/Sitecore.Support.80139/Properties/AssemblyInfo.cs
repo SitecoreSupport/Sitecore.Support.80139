7985589f3c533bbce1bc8034efae6b97e2c8d821
Anton Kechashin <atk@sitecore.net>
2017-04-04 14:03:58 +0300

    Initial commit.

---
+++ src/Sitecore.Support.80139/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.80139")]
+[assembly: AssemblyProduct("Sitecore.Support.80139")]
+[assembly: ComVisible(false)]
